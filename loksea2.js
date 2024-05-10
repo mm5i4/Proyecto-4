0ca3e016ce0bcf63019ced3951e08d228178d012
mm5i4 <marcos.mimiza@escuelacangallo.edu.ar>
2024-05-10 09:05:02 -0300

    first commit

---
+++ loksea2.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function(){
+          $("#Login").on("click",function(){
+            alert("hola")
+          })
+          $("#Borrar").on("click",funtion(
+            
+          ))
+
+    }
+)
+let value
