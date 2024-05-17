36aeb96bac139c9b29dc065970986f5d4cd48777
mm5i4 <marcos.mimiza@escuelacangallo.edu.ar>
2024-05-17 09:11:46 -0300

    Arreglamos el boton borrar

--- loksea2.js
+++ loksea2.js
@@ -1,11 +1,13 @@
 $(document).ready(
     function(){
-          $("#Login").on("click",function(){
-            let name = $("#usuario").val()
-            alert(name)
+          $("#login").on("click",function(){
+            //let name = $("#usuario").val()
+            if( $("#usuario").val() == ""){
+              alert("Falta ingresar el nombre de usuario")
+            } 
           })
-        
-
+          $("#borrar").on("click",function(){
+             $ ("input").val('') 
+          })
     }
 )
-let value
