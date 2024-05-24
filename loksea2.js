a13017ddc94561237707cb5c82ff479575c9cf46
mm5i4 <marcos.mimiza@escuelacangallo.edu.ar>
2024-05-24 09:19:02 -0300

    Seguimos con la página

--- loksea2.js
+++ loksea2.js
@@ -4,10 +4,20 @@ $(document).ready(
             //let name = $("#usuario").val()
             if( $("#usuario").val() == ""){
               alert("Falta ingresar el nombre de usuario")
-            } 
+              return
+            }
+            if( $("#password").val() == ""){
+              alert("Falta ingresar la contrasena")
+              return
+            }
+            if( $("#usuario").val() != "" && $("password").val() != ""){
+              window.location.href = "respuesta.html";
+              return
+            }
           })
           $("#borrar").on("click",function(){
              $ ("input").val('') 
           })
+          
     }
 )
